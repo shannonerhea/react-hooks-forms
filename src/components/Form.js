44673e7b260be18cbd53255f3b32469dbce18a40
shannone <shannone426@gmail.com>
2022-09-22 10:41:23 -0400

    updating state in form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,24 @@
 import React, { useState } from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
-  
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+function Form() {
+  const [number, setNumber] = useState(0);
+  const [isInvalidNumber, setIsInvalidNumber] = useState(null);
+
+  function handleNumberChange(event) {
+    const newNumber = parseInt(event.target.value);
+    if (newNumber >= 0 && newNumber <= 5) {
+      setNumber(newNumber);
+      setIsInvalidNumber(null);
+    } else {
+      setIsInvalidNumber(`${newNumber} is not a valid number!`);
+    }
   }
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
+      <input type="number" value={number} onChange={handleNumberChange} />
+      {isInvalidNumber ? <span style={{ color: "red" }}>{isInvalidNumber}</span> : null}
     </form>
   );
 }
---
+++ src/components/ParentComponent.js
@@ -0,0 +1,21 @@
+// src/components/ParentComponent
+import React, { useState } from "react";
+import Form from "./Form";
+import DisplayData from "./DisplayData";
+
+function ParentComponent() {
+  // ...
+  return (
+    <div>
+      <Form
+        firstName={firstName}
+        lastName={lastName}
+        handleFirstNameChange={handleFirstNameChange}
+        handleLastNameChange={handleLastNameChange}
+      />
+      <DisplayData firstName={firstName} lastName={lastName} />
+    </div>
+  );
+}
+
+export default ParentComponent;
